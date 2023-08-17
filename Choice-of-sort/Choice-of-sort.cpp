21c25e034e0cc9cdece395febf0cbddd02842a37
tokaretin <tokaretin@gmail.com>
2023-08-17 15:13:14 +0200

    Добавьте файлы проекта.

---
+++ Choice-of-sort/Choice-of-sort.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> vec = { 7,12,1,5,15,4,6,-20,100,5,3 };
+    
+    for  (int i = vec.size() - 1; i >=0; i--)
+    {
+        int maxind = 0;
+        for (int j = 0; j <= i; j++)
+        {
+            if (vec[j] > vec[maxind])
+                maxind = j;
+        }
+
+        int temp = vec[i];
+        vec[i] = vec[maxind];
+        vec[maxind] = temp;
+    }
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+
+    return 0;
+
+}
+
