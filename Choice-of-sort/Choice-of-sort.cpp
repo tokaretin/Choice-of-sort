4f1b15059cfd94267ca92a4d34ef7042a77b7a15
tokaretin <tokaretin@gmail.com>
2023-08-17 15:24:25 +0200

    std::swap

--- Choice-of-sort/Choice-of-sort.cpp
+++ Choice-of-sort/Choice-of-sort.cpp
@@ -1,11 +1,12 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main()
 {
     std::vector<int> vec = { 7,12,1,5,15,4,6,-20,100,5,3 };
     
-    for  (int i = vec.size() - 1; i >=0; i--)
+    for  (int i = vec.size() - 1; i >= 0; i--)
     {
         int maxind = 0;
         for (int j = 0; j <= i; j++)
@@ -14,9 +15,11 @@ int main()
                 maxind = j;
         }
 
-        int temp = vec[i];
+        /*int temp = vec[i];
         vec[i] = vec[maxind];
-        vec[maxind] = temp;
+        vec[maxind] = temp;*/
+
+        std::swap(vec[i], vec[maxind]);
     }
 
     for (int i = 0; i < vec.size(); i++)
